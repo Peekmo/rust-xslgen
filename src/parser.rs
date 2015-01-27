111adbed5f424823fb570034e5857fb0d3791804
Peekmo <axel.anceau@gmail.com>
2015-01-28 00:02:06 +0100

    Big refactor and added some comments (so genius)

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,7 @@
 use std::vec::Vec;
 
+/// ParserContext's different states
+/// So brillant !
 enum ParserContext {
     Empty,
     Tag,
@@ -8,6 +10,8 @@ enum ParserContext {
     InsideString
 }
 
+/// A node is a complete <xsl:when test="peekmo_qi > einstein_qi"> (for example)
+#[derive(Clone)]
 pub struct Node {
     pub name: String,
     pub namespace: Option<String>,
@@ -16,11 +20,15 @@ pub struct Node {
     pub parent: Option<Box<Node>>
 }
 
+/// A node attribute element (test="peekmo_qi > einstein_qi")
+#[derive(Clone)]
 pub struct Attribute {
     pub key: String,
     pub value: String
 }
 
+/// All the process structure
+/// <--- MAGIC IS STORED HERE !!!
 pub struct Parser {
     xslg_file: Box<Vec<String>>,
     current_attribute: Option<Box<Attribute>>,
@@ -31,18 +39,27 @@ pub struct Parser {
 }
 
 impl Node {
-    pub fn new() -> Self {
+    /// Creates the node with some params.
+    /// Parser is used to determine the parent (if any)
+    pub fn new(parser: &Parser, namespace: Option<String>, name: Option<String>) -> Self {
         Node {
-            name: String::new(),
-            namespace: None,
+            name: match name {
+                Some (n) => { n },
+                None     => String::new()
+            },
+            namespace: namespace,
             attributes: Vec::new(),
             children: None,
-            parent: None
+            parent: match parser.current_node {
+                Some (ref n) => { Some(n.clone()) },
+                None         => { None }
+            }
         }
     }
 }
 
 impl Attribute {
+    /// LOL
     pub fn new() -> Self {
         Attribute {
             key: String::new(),
@@ -52,6 +69,7 @@ impl Attribute {
 }
 
 impl Parser {
+    /// Nothing interesting
     pub fn new(xslg_file: Box<Vec<String>>) -> Self {
         Parser {
             xslg_file: xslg_file,
@@ -63,57 +81,94 @@ impl Parser {
         }
     }
 
+    /// Parse its xslg_file to store a Node representation of the file (to build the XSL file from
+    /// them !!)
+    /// <--- MAGIC HAPPENED HERE !!!
+    /// <--- HEARTH OF THE EARTH !!!
+    /// <--- THANK YOU LORD !!!
     pub fn parse(&mut self) {
-        for line in self.xslg_file.iter() {
+        // We will read all the lines.. Yes.. So boring.
+        for line in self.xslg_file.clone().iter() {
             self.buffer.clear();
+            self.context = ParserContext::Empty;
 
+            // A char by char work.. Yes.. That's life
+            // THUG LIFE
             for current_char in line.as_bytes().iter() {
                 let cha = *current_char as char;
 
                 match self.context {
-                    /**
-                     * --------------  Empty context !
-                     */
+                    // Empty context ? Yes we don't know where we are !
                     ParserContext::Empty => {
-                        match cha {
-                            '@' | '.' => {
-                                let mut node = Box::new(Node::new());
+                        self.parse_empty_context(cha);
+                    },
 
-                                node.namespace = match cha {
-                                    '@' => { Some(String::from_str("xsl")) },
-                                    '.' => { Some(self.buffer.clone()) },
-                                    _   => { None }
-                                };
-
-                                self.context = ParserContext::Tag;
-                                self.current_node = Some(node);
-                                self.buffer.clear();
-                            },
-                            ' ' => {
-                                match self.buffer.as_slice() {
-                                    "if" => {},
-                                    "elsif" => {},
-                                    "else" => {},
-                                    "" => {},
-                                    _ => {
-                                        let mut node = Box::new(Node::new());
-                                        node.name = self.buffer.clone();
-
-                                        self.context = ParserContext::Tag;
-                                        self.current_node = Some(node);
-                                        self.buffer.clear();
-                                    }
-                                }
-                            },
-                            _ => { self.buffer.push(cha); }
-                        }
-                    }
-                    _ => { panic!("Transpiler error -- Unknown context"); }
+                    // WTF ! Poor lazy man, do your job ! (Yes, I'm not paid for it but..)
+                    _ => { panic!("Transpiler error -- Unimplemented context"); }
                 }
-                self.buffer.push(*current_char as char);
             }
+        }
+    }
 
-            println!("{}", self.buffer);
+    /// A new char in an Empty context comes here to be judged.
+    fn parse_empty_context(&mut self, cha: char) {
+        match cha {
+            // '@' is an XSL tag
+            // '.' is a separator for namespace.tagname
+            //
+            // So... Let's build the tag !!!
+            '@' | '.' => {
+                let mut node = Box::new(Node::new(self, None, None));
+
+                node.namespace = match cha {
+                    '@' => { Some(String::from_str("xsl")) },
+                    '.' => { Some(self.buffer.clone()) },
+                    _   => { None }
+                };
+
+                self.context = ParserContext::Tag;
+                self.current_node = Some(node);
+                self.buffer.clear();
+            },
+
+            // A space is breaking the Empty context !!! DAAAAA
+            ' ' => {
+                // Let's have a look to the buffer
+                match self.buffer.as_slice() {
+                    // Nothing in the buffer ? False alert
+                    // It should never happened, but.. I'm not sure so.. :D
+                    "" => {},
+
+                    // Conditional expressions (for too perhaps ?)
+                    "if" | "elsif" | "else" => {
+                        let mut node = Box::new(Node::new(self, None, None));
+
+                    // TODO update here after antoine's answer (XSL MASTER)
+                    node.namespace = Some(String::from_str("xsl"));
+                    node.name =  String::from_str("when");
+
+                    self.context = ParserContext::Expression;
+                    self.current_node = Some(node);
+                    self.buffer.clear();
+                    },
+
+                    // Everything else is a tag without prefix
+                    _ => {
+                        let mut node = Box::new(
+                            Node::new(
+                                self,
+                                None,
+                                Some(self.buffer.clone())
+                                )
+                            );
+
+                        self.context = ParserContext::Tag;
+                        self.current_node = Some(node);
+                        self.buffer.clear();
+                    }
+                }
+            },
+            _ => { self.buffer.push(cha); }
         }
     }
 }
